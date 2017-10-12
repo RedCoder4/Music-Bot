d270e139c103eeff89b699f095a3857c9c2dfa0b
RedCoder4 <bd5@hotmail.com.au>
2017-10-12 16:58:45 +1100

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ exports.TOKEN = 'MzY2ODM4ODAyNjY2MjI1NjY1.DLzYQA.NAvhXUcRXzsOPJ0u6Qvuunkcv1Y';
 
 exports.PREFIX = 'radio ';
 
-exports.GOOGLE_API_KEY = '';
+exports.GOOGLE_API_KEY = 'AIzaSyCUbPXhvY5-EqDPpEUNf9aigbAM2zmjN8c';
